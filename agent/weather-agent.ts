0fae7bb13b32f8356e5a0b2bfc8b7573a40a9654
Lars Grammel <lars.grammel@gmail.com>
2025-09-30 15:56:51 +0200

    generic

--- agent/weather-agent.ts
+++ agent/weather-agent.ts
@@ -1,10 +1,12 @@
 import { weatherTool } from "@/tool/weather-tool";
 import { Agent } from "ai";
 
-export const weatherAgent = new Agent({
+const weatherAgent = new Agent({
   model: "openai/gpt-4o",
   system: "You are a helpful assistant.",
   tools: {
     weather: weatherTool,
   },
 });
+
+export default weatherAgent;
--- index.ts
+++ index.ts
@@ -1,8 +1,26 @@
-import { weatherAgent } from "@/agent/weather-agent";
 import { printStream } from "@/util/print-stream";
+import type { Agent, ToolSet } from "ai";
+import { parseArgs } from "util";
 
-const result = weatherAgent.stream({
-  prompt: "What is the weather in Tokyo?",
+const { values, positionals } = parseArgs({
+  args: Bun.argv,
+  options: {
+    agent: {
+      type: "string",
+    },
+  },
+  strict: true,
+  allowPositionals: true,
 });
+
+const agentPath = `./agent/${values.agent}-agent.ts`;
+
+const agent = (await import(agentPath)).default as Agent<
+  ToolSet,
+  unknown,
+  unknown
+>;
+
+const result = agent.stream({ prompt: positionals[2]! });
 
 await printStream(result);
