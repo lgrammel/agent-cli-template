27a9f225080b49a200f665517cfbbbf501ac2eb9
Lars Grammel <lars.grammel@gmail.com>
2025-09-30 15:44:29 +0200

    agent running

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { weatherAgent } from "./agent/weather-agent";
+
+const { text } = await weatherAgent.generate({
+  prompt: "What is the weather in Tokyo?",
+});
+
+console.log(text);
---
+++ tool/weather-tool.ts
@@ -0,0 +1,14 @@
+import { tool } from "ai";
+import { z } from "zod";
+
+export const weatherTool = tool({
+  description: "Get the weather in a location",
+  inputSchema: z.object({ city: z.string() }),
+  async execute({ city }) {
+    const weatherOptions = ["sunny", "cloudy", "rainy", "snowy", "windy"];
+    const weather =
+      weatherOptions[Math.floor(Math.random() * weatherOptions.length)];
+
+    return { temperature: 72, weather };
+  },
+});
